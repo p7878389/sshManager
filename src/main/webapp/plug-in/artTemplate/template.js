5dab75946433c0a98757e7a06e887a6921da397d
pxh <pxh@timevale.com>
2016-09-23 14:55:52 +0800

    Signed-off-by: pxh <pxh@timevale.com>

---
+++ src/main/webapp/plug-in/artTemplate/template.js
@@ -0,0 +1,98 @@
+/*TMODJS:{"version":"1.0.0"}*/
+!function () {
+    function a(a, b) {
+        return (/string|function/.test(typeof b) ? h : g)(a, b)
+    }
+
+    function b(a, c) {
+        return "string" != typeof a && (c = typeof a, "number" === c ? a += "" : a = "function" === c ? b(a.call(a)) : ""), a
+    }
+
+    function c(a) {
+        return l[a]
+    }
+
+    function d(a) {
+        return b(a).replace(/&(?![\w#]+;)|[<>"']/g, c)
+    }
+
+    function e(a, b) {
+        if (m(a))for (var c = 0, d = a.length; d > c; c++)b.call(a, a[c], c, a); else for (c in a)b.call(a, a[c], c)
+    }
+
+    function f(a, b) {
+        var c = /(\/)[^\/]+\1\.\.\1/, d = ("./" + a).replace(/[^\/]+$/, ""), e = d + b;
+        for (e = e.replace(/\/\.\//g, "/"); e.match(c);)e = e.replace(c, "/");
+        return e
+    }
+
+    function g(b, c) {
+        var d = a.get(b) || i({filename: b, name: "Render Error", message: "Template not found"});
+        return c ? d(c) : d
+    }
+
+    function h(a, b) {
+        if ("string" == typeof b) {
+            var c = b;
+            b = function () {
+                return new k(c)
+            }
+        }
+        var d = j[a] = function (c) {
+            try {
+                return new b(c, a) + ""
+            } catch (d) {
+                return i(d)()
+            }
+        };
+        return d.prototype = b.prototype = n, d.toString = function () {
+            return b + ""
+        }, d
+    }
+
+    function i(a) {
+        var b = "{Template Error}", c = a.stack || "";
+        if (c)c = c.split("\n").slice(0, 2).join("\n"); else for (var d in a)c += "<" + d + ">\n" + a[d] + "\n\n";
+        return function () {
+            return "object" == typeof console && console.error(b + "\n\n" + c), b
+        }
+    }
+
+    var j = a.cache = {}, k = this.String, l = {
+        "<": "&#60;",
+        ">": "&#62;",
+        '"': "&#34;",
+        "'": "&#39;",
+        "&": "&#38;"
+    }, m = Array.isArray || function (a) {
+            return "[object Array]" === {}.toString.call(a)
+        }, n = a.utils = {
+        $helpers: {}, $include: function (a, b, c) {
+            return a = f(c, a), g(a, b)
+        }, $string: b, $escape: d, $each: e
+    }, o = a.helpers = n.$helpers;
+    a.get = function (a) {
+        return j[a.replace(/^\.\//, "")]
+    }, a.helper = function (a, b) {
+        o[a] = b
+    }, "function" == typeof define ? define(function () {
+        return a
+    }) : "undefined" != typeof exports ? module.exports = a : this.template = a, /*v:1*/
+        a("personalCenter", function (a) {
+            "use strict";
+            var b = this, c = (b.$helpers, b.$escape), d = a.userName, e = a.passWord, f = a.salt, g = a.state, h = "";
+            return h += '<form class="form-horizontal"> <div class="form-group"> <label for="userName" class="col-lg-3 control-label">\u7528\u6237\u540d\uff1a</label> <div class="col-lg-7"> <input type="text" class="form-control" id="userName" value="', h += c(d), h += '" placeholder="\u8bf7\u8f93\u5165\u7528\u6237\u540d" readonly> </div> </div> <div class="form-group"> <label for="passWord" class="col-lg-3 control-label">\u5bc6\u7801\uff1a</label> <div class="col-lg-7"> <input type="password" class="form-control" id="passWord" value="', h += c(e), h += '" placeholder="\u8bf7\u8f93\u5bc6\u7801"> </div> </div> <div class="form-group"> <label for="salt" class="col-lg-3 control-label">\u76d0\uff1a</label> <div class="col-lg-7"> <input type="text" class="form-control" id="salt" value="', h += c(f), h += '" placeholder="\u8bf7\u8f93\u5165\u76d0"> </div> </div> <div class="form-group"> <label class="col-lg-3 control-label">\u89d2\u8272\uff1a</label> <div class="col-lg-7"> </div> </div> <div class="form-group"> <label class="col-lg-3 control-label">\u7528\u6237\u72b6\u6001\uff1a</label> <input type="hidden" id="state" value="', h += c(g), h += '"> <div class="col-lg-7"> <div class="form-control"> ', h += 0 == g ? " \u53ef\u7528 " : " \u9501\u5b9a ", h += " </div> </div> </div> </form>", new k(h)
+        }), /*v:1*/
+        a("userPageTemp", function (a) {
+            "use strict";
+            var b = this, c = (b.$helpers, b.$each), d = a.results, e = (a.user, a.index, b.$escape), f = "";
+            return f += '<tr class=""> <td>\u7528\u6237\u540d</td> <td>\u76d0</td> <td>\u7528\u6237\u72b6\u6001</td> <td>\u64cd\u4f5c</td> </tr> ', c(d, function (a, b) {
+                f += " ", f += 0 == b % 2 ? ' <tr class=""> ' : ' <tr class=""> ', f += " <td>", f += e(a.userName), f += "</td> <td>", f += e(a.salt), f += "</td> <td> ", f += 0 == a.state ? " \u53ef\u7528 " : " \u9501\u5b9a ", f += ' </td> <td> <span class="btn btn-primary btn-sm" onclick="userList.userUpdateModal(', f += e(a.userId), f += ')">\u4fee\u6539<span class="glyphicon glyphicon-pencil"></span></span> <span class="btn btn-danger btn-sm" onclick="userList.userDelete(', f += e(a.userId), f += ')">\u5220\u9664<span class="glyphicon glyphicon-trash"></span></span> </td> </tr> '
+            }), new k(f)
+        }), /*v:6*/
+        a("userUpdateTemp", function (a) {
+            "use strict";
+            var b = this, c = (b.$helpers, b.$escape), d = a.userId, e = a.userName, f = a.passWord, g = a.salt, h = "";
+            return h += '<div class="container-fluid"> <div class="row"> <form class="form-horizontal" role="form" id="userUpdateForm"> <input type="hidden" id="updateUserId" value="', h += c(d), h += '"/> <div class="form-group"> <label for="updateUserName" class="col-lg-3 col-lg-offset-1 control-label">\u7528\u6237\u540d\uff1a</label> <div class="col-lg-6"> <input type="text" class="form-control" id="updateUserName" readonly value="', h += c(e), h += '"> </div> </div> <div class="form-group"> <label for="updatePassWord" class="col-lg-3 col-lg-offset-1 control-label">\u5bc6\u7801\uff1a</label> <div class="col-lg-6"> <input type="password" class="form-control" value="', h += c(f), h += '" id="updatePassWord" placeholder="\u8bf7\u8f93\u5165\u5bc6\u7801"> </div> </div> <div class="form-group"> <label for="updateState" class="col-lg-3 col-lg-offset-1 control-label">\u7528\u6237\u72b6\u6001\uff1a</label> <div class="col-lg-6"> <select class="form-control" id="updateState"> <option value="0">\u53ef\u7528</option> <option value="1">\u9501\u5b9a</option> </select> </div> </div> <div class="form-group"> <label for="updateSalt" class="col-lg-3 col-lg-offset-1 control-label">\u76d0\uff1a</label> <div class="col-lg-6"> <input type="text" id="updateSalt" class="form-control" value="', h += c(g), h += '"> </div> </div> </form> </div> </div>', new k(h)
+        })
+}();
